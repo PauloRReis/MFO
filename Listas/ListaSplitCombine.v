a36df7cf10fd253845ef5c29e4891fa81f7c5b1d
Paulo Ricardo dos Reis <paulo230301@hotmail.com>
2023-10-08 22:49:01 -0300

    exs

---
+++ Listas/ListaSplitCombine.v
@@ -0,0 +1,47 @@
+Require Export Coq.Lists.List.
+Import ListNotations.
+
+Fixpoint combine {X Y : Type} (lx : list X) (ly : list Y)
+           : list (X*Y) :=
+  match lx, ly with
+  | [], _ => []
+  | _, [] => []
+  | x :: tx, y :: ty => (x, y) :: (combine tx ty)
+  end.
+
+Fixpoint split {X Y : Type} (l : list (X*Y))
+               : (list X) * (list Y) :=
+  match l with
+  | [] => ([], [])
+  | (x, y) :: t =>
+      match split t with
+      | (lx, ly) => (x :: lx, y :: ly)
+      end
+  end.
+
+Lemma eq_cons : forall (X:Type) (l1 l2: list X) (x: X),
+  l1 = l2 -> x :: l1 = x :: l2.
+  intros X l1 l2 x.
+  intro H.
+  rewrite H.
+  reflexivity.
+Qed.
+
+Theorem combine_split : forall X Y (l : list (X * Y)) l1 l2,
+  split l = (l1, l2) ->
+  combine l1 l2 = l.
+Proof.
+  intros X Y l. induction l as [ | [x y] l'].
+  - intros l1 l2 H. simpl in H. injection H as H1 H2.
+  rewrite <- H1. rewrite <- H2. reflexivity.
+  - intros l1 l2 H. simpl in H. destruct (split) as [ t1 t2 ].
+  inversion H. simpl. rewrite IHl'.
+    + reflexivity.
+    + reflexivity.
+Qed.
+
+
+Theorem split_combine : forall X Y (l1 : list X) (l2 : list Y) (l : list (X*Y)),
+  length l1 = length l2 -> combine l1 l2 = l -> split l = (l1, l2).
+Proof.
+  Admitted.
---
+++ Livro/lf/.Makefile.d
@@ -0,0 +1,21 @@
+Basics.vo Basics.glob Basics.v.beautified Basics.required_vo: Basics.v 
+Basics.vio: Basics.v 
+Basics.vos Basics.vok Basics.required_vos: Basics.v 
+IndProp.vo IndProp.glob IndProp.v.beautified IndProp.required_vo: IndProp.v Logic.vo
+IndProp.vio: IndProp.v Logic.vio
+IndProp.vos IndProp.vok IndProp.required_vos: IndProp.v Logic.vos
+Induction.vo Induction.glob Induction.v.beautified Induction.required_vo: Induction.v Basics.vo
+Induction.vio: Induction.v Basics.vio
+Induction.vos Induction.vok Induction.required_vos: Induction.v Basics.vos
+Lists.vo Lists.glob Lists.v.beautified Lists.required_vo: Lists.v Induction.vo
+Lists.vio: Lists.v Induction.vio
+Lists.vos Lists.vok Lists.required_vos: Lists.v Induction.vos
+Logic.vo Logic.glob Logic.v.beautified Logic.required_vo: Logic.v Tactics.vo
+Logic.vio: Logic.v Tactics.vio
+Logic.vos Logic.vok Logic.required_vos: Logic.v Tactics.vos
+Poly.vo Poly.glob Poly.v.beautified Poly.required_vo: Poly.v Lists.vo
+Poly.vio: Poly.v Lists.vio
+Poly.vos Poly.vok Poly.required_vos: Poly.v Lists.vos
+Tactics.vo Tactics.glob Tactics.v.beautified Tactics.required_vo: Tactics.v Poly.vo
+Tactics.vio: Tactics.v Poly.vio
+Tactics.vos Tactics.vok Tactics.required_vos: Tactics.v Poly.vos
